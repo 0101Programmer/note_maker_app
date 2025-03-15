414d210d43aff16ffb04b3380d895d2397885589
Владимир <baranov-v-k@yandex.ru>
2025-03-15 15:57:22 +0300

    Оптимизация кода

--- redis_config.py
+++ redis_config.py
@@ -1,20 +1,10 @@
 import redis
 import os
 
-from dotenv import load_dotenv
-
 # Создаем подключение к Redis
 redis_client = redis.StrictRedis(
     host=os.getenv("REDIS_HOST"),  # Адрес сервера Redis
     port=os.getenv("REDIS_PORT"),         # Порт Redis (по умолчанию 6379)
     db=os.getenv("REDIS_DB"),              # Номер базы данных (по умолчанию 0)
     decode_responses=os.getenv("REDIS_DECODE_RESPONSES")  # Автоматически декодировать ответы в строки
-)
-
-# Временное подключение к Redis для FastAPI
-docker_off_redis_client = redis.StrictRedis(
-    host='localhost',  # Адрес сервера Redis
-    port=6379,         # Порт Redis (по умолчанию 6379)
-    db=0,              # Номер базы данных (по умолчанию 0)
-    decode_responses=True  # Автоматически декодировать ответы в строки
 )
--- start_app_script.py
+++ start_app_script.py
@@ -1,23 +1,13 @@
 import subprocess
 
-fastapi_main = 'python -m backend.api.main'
-bot = 'python -m backend.bot.bot'
-docker = 'docker-compose -f docker/docker-compose.yml --env-file .env up --build'
+# Команда для запуска Docker Compose
+docker = 'docker-compose -f docker/docker-compose.yml up --build'
 
-# Запуск команд параллельно
+# Запуск приложения
 try:
-    print("Запуск FastAPI...")
-    fastapi_process = subprocess.Popen(fastapi_main, shell=True)
-
-    # print("Запуск Telegram Bot...")
-    # bot_process = subprocess.Popen(bot, shell=True)
-
     print('Запуск Docker-compose...')
     docker_process = subprocess.Popen(docker, shell=True)
 
-    # Ожидание завершения процессов
-    fastapi_process.wait()
-    # bot_process.wait()
     docker_process.wait()
 except Exception as e:
     print(f"Ошибка при выполнении команды: {e}")
